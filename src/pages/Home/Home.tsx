e7ea4943e9622118d7fe2e08a24212fe641dd5a8
Joel Rdz <joelrdzh@gmail.com>
2023-06-16 18:40:05 -0600

    adding DataGrid, basic example with People dummy
    - starting with one column
    - patch: color-scheme inversion

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,28 @@
+import { People } from '@/data';
+import { DataGrid, GridRenderCellParams } from '@mui/x-data-grid';
+
 export type HomeProps = {}
 
 const Home: React.FC<HomeProps> = () => {
-	return <div>Home</div>;
+  const pageSize = 5;
+  const columns = [
+    {
+      field: 'name',
+      headerName: 'Name',
+      flex: 1,
+      minWidth: 150,
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+    },
+  ];
+  return (
+    <DataGrid
+      rows={People}
+      columns={columns}
+      initialState={{
+        pagination: { paginationModel: { pageSize } }
+      }}
+    />
+  );
 };
 
 export default Home;
